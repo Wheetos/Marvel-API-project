f150e27d6e8a58b03c953e1f00ce62f7f510e9dd
Julio Andrade-Jimenez <julioandrade20943@gmail.com>
2024-07-11 14:37:10 +0000

    Changed the http header to https

--- marvel-api-project/src/hooks/Fetch.jsx
+++ marvel-api-project/src/hooks/Fetch.jsx
@@ -1,11 +1,11 @@
 import {useState, useEffect} from 'react';
 /*This Fetch function uses the useEffect() to fetch the Marvel API hash
-and then returns it back as a json. It then logs the json into a file, and 
-also puts the json data into the setMarvel const */
+and then returns it back as a json. It then logs the json, and 
+also puts it into the setMarvel const */
 const Fetch = () => {
     const [marvel, SetMarvel] = useState([]);
         useEffect(() => {
-            fetch('http://gateway.marvel.com/v1/public/characters?limit=100&ts=1&apikey=869df33cbed7f29ac109149da9ccf83e&hash=d43a9e6097fe7b58c5be2871a3983e8c')
+            fetch('https://gateway.marvel.com/v1/public/characters?limit=100&ts=1&apikey=869df33cbed7f29ac109149da9ccf83e&hash=d43a9e6097fe7b58c5be2871a3983e8c')
             .then((res) => {
         return res.json();
         })
