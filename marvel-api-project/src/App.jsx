78249ecfc588b2142abec58ff400bc5bc67c79f0
Julio Andrade-Jimenez <julioandrade20943@gmail.com>
2024-07-12 20:20:48 +0000

    Added a Hero banner

--- marvel-api-project/src/App.jsx
+++ marvel-api-project/src/App.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
-import Fetch from "./hooks/Fetch";
 
 function App() {
   return (
     <React.Fragment>
       <Header></Header>
       <Home></Home>
-      <Fetch></Fetch>
       <Footer></Footer>
     </React.Fragment>
   );
--- marvel-api-project/src/components/Home.jsx
+++ marvel-api-project/src/components/Home.jsx
@@ -1,10 +1,25 @@
 import * as React from "react";
+import "../styles/main.css";
 
 function Home() {
   return (
     <React.Fragment>
-      <h1>Marvel Characters</h1>
-      <h4>Get hooked on a hearty helping of heroes and villians.</h4>
+      <div className="hero-container">
+      <div className="wrapper-img">
+        <div className="wrapper-header">
+          <h1>Welcome to Marvel!</h1>
+          <h4>
+            Sit back, relax, and take a look at all of the heroes and villians
+            here!
+          </h4>
+        </div>
+        </div>
+      </div>
+      <div className="wrapper">
+        <div className="wrapper-featured">
+          <h3>Featured characters</h3>
+        </div>
+      </div>
     </React.Fragment>
   );
 }
