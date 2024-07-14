06c59eb6640c9992d62d7688e821db4a94f837f2
Julio Andrade-Jimenez <julioandrade20943@gmail.com>
2024-07-13 23:22:35 -0400

    Fixed some CSS & added a couple of characters for the Feat.jsx component

--- marvel-api-project/src/hooks/Feat.jsx
+++ marvel-api-project/src/hooks/Feat.jsx
@@ -1,8 +1,12 @@
 import { useState, useEffect } from "react";
 import * as React from "react";
+import "../styles/card.css";
 const Feat = () => {
     const [spidey, setSpidey] = useState([]);
     const [hulk, SetHulk] = useState([]);
+    const [wolverine, SetWolveirne] = useState([]);
+    const [ironman, SetIronman] = useState([]);
+    const [thor, SetThor] = useState([]);
     useEffect(() => {
         const request1 = fetch(
           "https://gateway.marvel.com/v1/public/characters?name=Spider-Man%20(Peter%20Parker)&ts=1&apikey=869df33cbed7f29ac109149da9ccf83e&hash=d43a9e6097fe7b58c5be2871a3983e8c"
@@ -10,10 +14,22 @@ const Feat = () => {
         const request2 = fetch(
           "https://gateway.marvel.com/v1/public/characters?name=Hulk&ts=1&apikey=869df33cbed7f29ac109149da9ccf83e&hash=d43a9e6097fe7b58c5be2871a3983e8c"
         ).then((response) => response.json());
+        const request3 = fetch(
+          "https://gateway.marvel.com/v1/public/characters?name=Wolverine&ts=1&apikey=869df33cbed7f29ac109149da9ccf83e&hash=d43a9e6097fe7b58c5be2871a3983e8c"
+        ).then((response) => response.json());
+        const request4 = fetch(
+          "https://gateway.marvel.com/v1/public/characters?name=Iron%20Man&ts=1&apikey=869df33cbed7f29ac109149da9ccf83e&hash=d43a9e6097fe7b58c5be2871a3983e8c"
+        ).then((response) => response.json());
+        const request5 = fetch(
+          "https://gateway.marvel.com/v1/public/characters?name=Thor&ts=1&apikey=869df33cbed7f29ac109149da9ccf83e&hash=d43a9e6097fe7b58c5be2871a3983e8c"
+        ).then((response) => response.json());
     
-        Promise.all([request1, request2]).then(([data1, data2]) => {
+        Promise.all([request1, request2, request3, request4, request5]).then(([data1, data2, data3, data4, data5]) => {
           setSpidey(data1.data.results);
           SetHulk(data2.data.results);
+          SetWolveirne(data3.data.results);
+          SetIronman(data4.data.results);
+          SetThor(data5.data.results);
         });
       }, []);
 
@@ -32,6 +48,39 @@ const Feat = () => {
               </div>
             ))}
             {hulk.map((data) => (
+              <div key={data.id}>
+                <div className="card">
+                  <img
+                    src={`${data.thumbnail.path}.${data.thumbnail.extension}`}
+                    alt={data.name}
+                  />
+                  <p>{data.name}</p>
+                </div>
+              </div>
+            ))}
+            {wolverine.map((data) => (
+              <div key={data.id}>
+                <div className="card">
+                  <img
+                    src={`${data.thumbnail.path}.${data.thumbnail.extension}`}
+                    alt={data.name}
+                  />
+                  <p>{data.name}</p>
+                </div>
+              </div>
+            ))}
+            {ironman.map((data) => (
+              <div key={data.id}>
+                <div className="card">
+                  <img
+                    src={`${data.thumbnail.path}.${data.thumbnail.extension}`}
+                    alt={data.name}
+                  />
+                  <p>{data.name}</p>
+                </div>
+              </div>
+            ))}
+            {thor.map((data) => (
               <div key={data.id}>
                 <div className="card">
                   <img
