41bd327810aee1a53713428008f20073c9ef1990
Julio Andrade-Jimenez <julioandrade20943@gmail.com>
2024-07-16 17:51:17 +0000

    Currently working on a general character page

--- marvel-api-project/src/pages/Characters.jsx
+++ marvel-api-project/src/pages/Characters.jsx
@@ -1,14 +1,16 @@
 import * as React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { Outlet } from "react-router-dom";
 
 function Characters() {
   return (
     <React.Fragment>
       <Header />
       <nav className="flexbox">
-      <p>Hello!</p>
-      <Footer />
+        <h1>Character List</h1>
+        <Outlet />
+        <Footer />
       </nav>
     </React.Fragment>
   );
